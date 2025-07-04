8cae815cae267f6b401ed9b9268f398716d2b2a0
felix-pc-bar <felix.pc.barber@outlook.com>
2025-07-04 17:22:29 +0100

    empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,60 @@
-#include <SDL.h>
 #include <iostream>
+#include <SDL.h>
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(0) != 0) {
-        std::cout << "SDL failed to init: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+// You shouldn't really use this statement, but it's fine for small programs
+using namespace std;
 
-    std::cout << "SDL initialized, cout works" << std::endl;
-    SDL_Quit();
-    return 0;
+// You must include the command line parameters for your main function to be recognized by SDL
+int main(int argc, char** args) {
+
+	// Pointers to our window and surface
+	SDL_Surface* winSurface = NULL;
+	SDL_Window* window = NULL;
+
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		cout << "Error initializing SDL: " << SDL_GetError() << endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Create our window
+	window = SDL_CreateWindow("Barbershop", 0, 0, 1920, 1080, SDL_WINDOW_FULLSCREEN_DESKTOP);
+	// Make sure creating the window succeeded
+	if (!window) {
+		cout << "Error creating window: " << SDL_GetError() << endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Get the surface from the window
+	winSurface = SDL_GetWindowSurface(window);
+
+	// Make sure getting the surface succeeded
+	if (!winSurface) {
+		cout << "Error getting surface: " << SDL_GetError() << endl;
+		system("pause");
+		// End the program
+		return 1;
+	}
+
+	// Fill the window with a white rectangle
+	SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 255, 255));
+
+	// Update the window display
+	SDL_UpdateWindowSurface(window);
+
+	// Wait
+	system("pause");
+
+	// Destroy the window. This will also destroy the surface
+	SDL_DestroyWindow(window);
+
+	// Quit SDL
+	SDL_Quit();
+
+	// End the program
+	return 0;
 }
