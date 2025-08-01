e444874a21e28f13b8c65ce8c1bc636a170c2590
felix <felix.pc.barber@outlook.com>
2025-08-01 16:57:54 +0100

    charlie's stuff

--- src/main.cpp
+++ src/main.cpp
@@ -54,12 +54,12 @@ int main(int argc, char** args) {
 	//mainScene.cams[0].rot.pitch += pi / 2;
 	mainScene.meshes.emplace_back(importObj("content/obj/raven.obj"));
 	mainScene.meshes[0].materials.emplace_back(0.15f, 0.15f, 0.15f);
-	mainScene.meshes.emplace_back(importObj("content/obj/terrain2.obj"));
+	mainScene.meshes.emplace_back(importObj("content/obj/fTerrain.obj"));
 	mainScene.meshes[1].materials.emplace_back(1.0f, 0.7f, 0.5f);
-	mainScene.meshes.emplace_back(importObj("content/obj/cacti.obj"));
+	mainScene.meshes.emplace_back(importObj("content/obj/fCacti.obj"));
 	mainScene.meshes[2].materials.emplace_back(0.25f, 0.95f, 0.3f);
-	mainScene.meshes.emplace_back(importObj("content/obj/rays.obj"));
-	mainScene.meshes[3].materials.emplace_back(0.33f, 0.6f, 1.0f);
+	mainScene.meshes.emplace_back(importObj("content/obj/fRocks.obj"));
+	mainScene.meshes[3].materials.emplace_back(0.33f, 0.33f, 0.33f);
 
 	const Uint8* gk; 
 	SDL_Event event;
@@ -76,7 +76,7 @@ int main(int argc, char** args) {
 	Quaternion camLookOffset(0.2f, { 1, 0, 0 }); // no rotation, or maybe slight pitch down if needed
 	mainScene.cams[0].quatIdentity = mainScene.meshes[0].quatIdentity * camLookOffset;
 
-	float flightSpeed = 0;
+	float flightSpeed = 1;
 
 	bb3d testbb({ -10, -100, -10 }, { 10, 100, 10 });
 
@@ -104,14 +104,16 @@ int main(int argc, char** args) {
 		if (gk[SDL_SCANCODE_D]) {	mainScene.meshes[0].rotateAxis(-0.03, {mainScene.meshes[0].forward}); }
 		if (gk[SDL_SCANCODE_S]) {	mainScene.meshes[0].rotateAxis(-0.03, {mainScene.meshes[0].right}); }
 		if (gk[SDL_SCANCODE_W]) {	mainScene.meshes[0].rotateAxis(0.03, {mainScene.meshes[0].right}); }
-		if (gk[SDL_SCANCODE_SPACE]) {	mainScene.meshes[0].rotateAxis(0.03, {mainScene.meshes[0].right}); }
+		if (gk[SDL_SCANCODE_LSHIFT]) { flightSpeed += 0.01f; }
+		if (gk[SDL_SCANCODE_LCTRL] && flightSpeed > 0.01f) { flightSpeed -= 0.01f; }
+	
 
 
 		mainScene.cams[0].quatIdentity = mainScene.meshes[0].quatIdentity * camLookOffset;
 
 		//if (gk[SDL_SCANCODE_A] && mainScene.meshes[0].quatIdentity) {}
 
-		flightSpeed = std::max(std::min(2.0l, (75.0f - mainScene.meshes[0].position.y) / 80.0l), 1.0l);
+		//flightSpeed = std::max(std::min(2.0l, (75.0f - mainScene.meshes[0].position.y) / 80.0l), 1.0l);
 		mainScene.meshes[0].move(mainScene.meshes[0].forward * flightSpeed);
 		
 		Position3d camOffset(0, 4, -10);
