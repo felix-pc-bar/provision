768754256efe2c675c265f050eff696d3ba863a8
felix <felix.pc.barber@outlook.com>
2025-08-01 12:35:23 +0100

    raven working

--- src/main.cpp
+++ src/main.cpp
@@ -51,28 +51,12 @@ int main(int argc, char** args) {
 	mainScene.currentCam = &mainScene.cams[0]; // Set mainScene's current camera to the camera we just created
 	mainScene.cams[0].pos.z -= 2;
 	//mainScene.cams[0].rot.pitch += pi / 2;
-	mainScene.meshes.emplace_back(importObj("content/obj/ship2.obj"));
-	mainScene.meshes[0].materials.emplace_back(0.8f, 0.8f, 0.9f);
+	mainScene.meshes.emplace_back(importObj("content/obj/raven.obj"));
+	mainScene.meshes[0].materials.emplace_back(0.15f, 0.15f, 0.15f);
 	mainScene.meshes.emplace_back(importObj("content/obj/terrain2.obj"));
 	mainScene.meshes[1].materials.emplace_back(1.0f, 0.7f, 0.5f);
 	mainScene.meshes.emplace_back(importObj("content/obj/cacti.obj"));
 	mainScene.meshes[2].materials.emplace_back(0.25f, 0.95f, 0.3f);
-	
-
-	mainScene.meshes.emplace_back(importObj("content/obj/raven_body.obj"));
-	mainScene.meshes[3].materials.emplace_back(0.15f, 0.15f, 0.15f);
-	mainScene.meshes.emplace_back(importObj("content/obj/raven_legs.obj"));
-	mainScene.meshes[4].move({0.925, -0.61, 0});
-	mainScene.meshes[4].materials.emplace_back(0.15f, 0.15f, 0.15f);
-	mainScene.meshes[3].children.emplace_back(&mainScene.meshes[4]);
-	mainScene.meshes.emplace_back(importObj("content/obj/raven_wing_l.obj"));
-	mainScene.meshes[5].move({0.034, 0.78, -0.59});
-	mainScene.meshes[5].materials.emplace_back(0.15f, 0.15f, 0.15f);
-	mainScene.meshes[3].children.emplace_back(&mainScene.meshes[5]);
-	mainScene.meshes.emplace_back(importObj("content/obj/raven_wing_r.obj"));
-	mainScene.meshes[6].move({0.034, 0.78, 0.59});
-	mainScene.meshes[6].materials.emplace_back(0.15f, 0.15f, 0.15f);
-	mainScene.meshes[3].children.emplace_back(&mainScene.meshes[6]);
 
 	const Uint8* gk; 
 	SDL_Event event;
@@ -81,8 +65,11 @@ int main(int argc, char** args) {
 	int frame = 0;
 
 	Quaternion qDelta(pi / 200, { 0,1,0 });
-	mainScene.meshes[0].rotateQuat({ -pi / 4, -1, 0, 0 });
+	//mainScene.meshes[0].rotateQuat({ -pi / 4, -1, 0, 0 });
 	mainScene.meshes[0].move({ -100,10,0 });
+
+	Quaternion camLookOffset(0.2f, { 1, 0, 0 }); // no rotation, or maybe slight pitch down if needed
+	mainScene.cams[0].quatIdentity = mainScene.meshes[0].quatIdentity * camLookOffset;
 
 	while (true)
 	{
@@ -98,7 +85,6 @@ int main(int argc, char** args) {
 			{
 				mainScene.meshes[0].rotateAxis((float)event.motion.xrel / 1000.0f, mainScene.meshes[0].up);
 				mainScene.meshes[0].rotateAxis((float)event.motion.yrel / 1000.0f, mainScene.meshes[0].right);
-				Quaternion camLookOffset(0, { 0, 1, 0 }); // no rotation, or maybe slight pitch down if needed
 				mainScene.cams[0].quatIdentity = mainScene.meshes[0].quatIdentity * camLookOffset;
 			}
 		}
@@ -107,7 +93,7 @@ int main(int argc, char** args) {
 		if (gk[SDL_SCANCODE_W]) { mainScene.meshes[0].move(mainScene.meshes[0].forward * 1.0f); }
 		if (gk[SDL_SCANCODE_S]) { mainScene.meshes[0].move(mainScene.meshes[0].forward * -1.0f); }
 		
-		Position3d camOffset(0, 2, -5);
+		Position3d camOffset(0, 4, -10);
 		camOffset.rotateQuat(mainScene.meshes[0].quatIdentity);
 		mainScene.cams[0].pos = mainScene.meshes[0].position + camOffset;
 
